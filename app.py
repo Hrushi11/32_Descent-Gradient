541f525b815aaedde932524d425f62348c967405
Prathamesh <paritprathamesh23@gmail.com>
2022-09-25 19:56:48 +0530

    added some templates

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+import webbrowser
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    webbrowser.open_new('http://127.0.0.1:2000/')
+    app.run(debug=True, port=2000)
